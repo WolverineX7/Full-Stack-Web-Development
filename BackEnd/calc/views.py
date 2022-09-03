6668665d570a5d4632ee22e85e6f440168a6b4d1
WolverineX7 <65910406+WolverineX7@users.noreply.github.com>
2022-09-03 12:07:54 +0530

    first commit

--- BackEnd/calc/views.py
+++ BackEnd/calc/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User,auth
 
 # Create your views here.
 def home(requests):
@@ -7,10 +8,43 @@ def home(requests):
     return render(requests,'home.html',{'name':first_name , 'age' : age})
 
 def login(requests):
-    return render(requests,'login.html')
+    if requests.method == 'POST':
+        username=requests.POST.get('username')
+        password = requests.POST.get('password')
+
+        user=auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(requests,user)
+            print("Login successful")
+            return redirect('/')
+    else:
+        return render(requests,'login.html')
 
 def signup(requests):
-    return render(requests,'signup.html')
+    if requests.method == 'POST':
+        firstname = requests.POST.get('first_name')
+        lastname = requests.POST.get('last_name')
+        email = requests.POST.get('email')
+        username = requests.POST.get('username')
+        password = requests.POST.get('password')
+        password2 = requests.POST.get('password2')
+
+        if password == password2:
+
+            user = User.objects.create_user(
+                username = username, 
+                email = email,
+                password = password,
+                first_name = firstname,
+                last_name = lastname
+            )
+            print("User created")
+            return redirect('/')
+        else:
+            print("Wrong password")
+
+    else: 
+        return render(requests,'signup.html')
 
 def add(requests):
     val1 = int(requests.POST['num1'])
